f9bf25eb1a53fb98b748282712087d6d0d69831a
juykyung.kim <jukyung.kim@nhn.com>
2018-08-08 21:15:08 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				아자아자
+			</div>
+		);
+	}
+}
+
+export default App;
